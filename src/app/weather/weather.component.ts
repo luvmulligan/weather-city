e3d05eaa4d8038c0eb485ec38849bd2ca0918362
luvmulligan <luciavelardemulligan@gmail.com>
2022-03-27 15:37:17 -0300

    more images

--- src/app/weather/weather.component.ts
+++ src/app/weather/weather.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormGroup, FormBuilder } from '@angular/forms';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 import { WeatherService } from '../weather.service';
 
 @Component({
@@ -28,29 +28,39 @@ export class WeatherComponent implements OnInit {
   }
 
   weatherQuery(formValues: any){
-    this.weatherService.getWeather(formValues.location).subscribe(data => {
-      this.weatherData = data;
-      console.log(this.weatherData);
-      this.setWeatherImage();
-    });
+    if (formValues.location === ''){
+      return
+    } else {
+      this.weatherService.getWeather(formValues.location).subscribe(data => {
+        this.weatherData = data;
+        console.log(this.weatherData);
+        this.setWeatherImage();
+      });
+    }
+
   }
   setWeatherImage(){
     const weatherDescription = this.weatherData.current.weather_descriptions[0]
-    if(weatherDescription === 'Sunny' || weatherDescription === 'Clear'){
+    if (weatherDescription === 'Sunny' || weatherDescription === 'Clear'){
       this.weatherImageUrl = './assets/001-sunny.png'
     } else if (weatherDescription === 'Partly cloudy'){
       this.weatherImageUrl = './assets/009-cloudy-2.png'
-    } else if (weatherDescription === 'Shower In Vicinity'){
+    } else if (weatherDescription === 'Shower In Vicinity'  || weatherDescription === 'Light rain' || weatherDescription === 'Moderate rain at times' || weatherDescription === 'Moderate rain'){
       this.weatherImageUrl = './assets/003-rainy.png'
-    } else if (weatherDescription === 'Mist'){
-      this.weatherImageUrl = './assets/drop.png'
+    } else if (weatherDescription === 'Heavy rain at times' || weatherDescription === 'Heavy rain'){
+      this.weatherImageUrl = './assets/008-rain.png'
+    } else if (weatherDescription === 'Mist' || weatherDescription === 'Haze'){
+      this.weatherImageUrl = './assets/mist.png'
     } else if (weatherDescription === 'Patchy light rain' || weatherDescription === 'Patchy rain possible'){
       this.weatherImageUrl = './assets/rain.png'
     } else if (weatherDescription === 'Cloudy' || weatherDescription === 'Fog' || weatherDescription === 'Overcast'){
       this.weatherImageUrl = './assets/010-cloud.png'
     } else if (weatherDescription == 'Thundery outbreaks possible'){
       this.weatherImageUrl = './assets/006-thunder.png'
+    } else if (weatherDescription === 'Light Snow, Snow' || weatherDescription === 'Snow' || weatherDescription === 'Blowing snow' || weatherDescription === 'Blizzard'){
+      this.weatherImageUrl = './assets/005-snow.png'
+    } else if (weatherDescription === 'Patchy snow possible'){
+      this.weatherImageUrl = './assets/snowing.png'
     }
   }
-
 }
