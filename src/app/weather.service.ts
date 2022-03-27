d2720d87780328287a66831af1ef171863498839
luvmulligan <luciavelardemulligan@gmail.com>
2022-03-26 22:30:54 -0300

    security fix

--- src/app/weather.service.ts
+++ src/app/weather.service.ts
@@ -1,19 +1,18 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { API_KEY } from './config';
 
 
 @Injectable({
   providedIn: 'root'
 })
 export class WeatherService {
-API_KEY='dda968728e4ca24c03f71ca9bea978bd'
-
 
   constructor(private http: HttpClient) { }
 
   getWeather(location: string){
     return this.http.get(
-      `http://api.weatherstack.com/current?access_key=${this.API_KEY}&query=${location}`
+      `http://api.weatherstack.com/current?access_key=${API_KEY}&query=${location}`
     )
 
   }
